32ac25cc11a9406632e9726abb25a044b459be8d
folkefd <folkefd@gmail.com>
2016-09-23 14:39:47 +0200

    Övning 2.4

--- While-loopar/Program.cs
+++ While-loopar/Program.cs
@@ -9,7 +9,7 @@ namespace While_loopar
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             //Övning 1
             /*
             int n = 0;
@@ -42,7 +42,61 @@ namespace While_loopar
             }
             Console.ReadKey();
             */
-            //Övning 3
+            //Övning 2.1
+            /*
+            int i = 1;
+            while (i++ <= 100)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            */
+            //Övning 2.2
+            /*
+            int i = 10;
+            while (i-- >= 1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            */
+            //Övning 2.3
+            /*
+            while (true)
+            {
+                Console.WriteLine("INFINITYYYYY!!!!!!11");
+            }
+            */
+            //Övning 2.4
+
+            Console.WriteLine("Gissa ett tal mellan 1 och 100: ");
+            Random rnd = new Random();
+            bool villkår = true;
+            int tal = rnd.Next(100);
+            int i = 0;
+            while (villkår)
+            {
+                int gissning = int.Parse(Console.ReadLine());
+                if (gissning < tal)
+                {
+                    Console.WriteLine("För lågt");
+                }
+
+                else if (gissning > tal)
+                {
+                    Console.WriteLine("För högt");
+                }
+
+                else
+                {
+                    Console.WriteLine("Det tog " + i + " försök!");
+                    villkår = false;
+                }
+                i++;
+
+            }
+            Console.ReadKey();
+            
 
         }//Main
     }//class
